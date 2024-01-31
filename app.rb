fd92a0733f0dd68cda81970ab26aecfa34490f97
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-01-31 23:08:01 +0000

    api works

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ end
 
 
 get("/holiday") do
-@api_key = ENV.fetch("API_KEY")
- 
+  api_key = ENV.fetch("API_KEY")
+  raw_response = HTTP.get("https://calendarific.com/api/v2/holidays?&api_key=#{api_key}&country=US&year=2019")
+  
+
+  parsed = JSON.parse(raw_response)
+  @holidays = parsed.fetch("response").fetch("holidays")
   erb(:holidayfinder)
 end
